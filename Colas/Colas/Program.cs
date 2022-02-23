0fab92df4807d75d1c6082f30f692ac45a4b6982
Frank-Rj <87914653+Frank-Rj@users.noreply.github.com>
2022-02-22 20:12:02 -0600

    Commit

---
+++ Colas/Colas/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+
+namespace Colas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Definiendo una Queue.
+            Queue numeros = new Queue();
+
+            Console.WriteLine("Ingresando elementos a la Cola: ");
+            numeros.Enqueue(Console.ReadLine());
+            numeros.Enqueue(Console.ReadLine());
+            numeros.Enqueue(Console.ReadLine());
+            numeros.Enqueue(Console.ReadLine());
+            numeros.Enqueue(Console.ReadLine());
+
+            //Mostrando las propiedades y valores de Queue.
+            Console.WriteLine("\nCantidad de elementos en la cola: {0}", numeros.Count);
+            Console.Write("\nValores dentro de la Queue: ");
+            
+            PrintValues(numeros);
+
+            // Eliminar  un elemento
+            Console.WriteLine("\nElemento a eliminar: {0}", numeros.Peek());
+            Console.WriteLine("Eliminando el elemento: {0}", numeros.Dequeue());
+
+            // Mostrar valores de la Queue.
+            Console.Write("\nValores de la Queue:");
+            PrintValues(numeros);
+
+            // Eliminar otros elementos
+            Console.WriteLine("\nSiguiente elemento a eliminar: {0}", numeros.Peek());
+            Console.WriteLine("Eliminando el elemento: {0}", numeros.Dequeue());
+
+            Console.WriteLine("\nUltimo elemento a eliminar: {0}", numeros.Peek());
+            Console.WriteLine("Eliminando el elemento: {0}", numeros.Dequeue());
+
+            // Mostrar valores de la Queue.
+            Console.Write("\nValores de la Queue: ");
+            PrintValues(numeros);
+
+            // Mostrar el primer valor la Queue
+            Console.WriteLine("\nPrimer elemento de la Queue: {0}", numeros.Peek());
+
+            Console.Write("\nValores de la Queue: ");
+            PrintValues(numeros);
+
+            numeros.Clear();
+
+            Console.WriteLine("\nValores de la Queue después de limpiar: {0}", numeros.Count);
+            PrintValues(numeros);
+
+            void PrintValues(IEnumerable myCollection)
+            {
+                foreach (Object obj in myCollection)
+
+                Console.Write("\t {0}", obj);
+                Console.WriteLine();        
+            }
+        }
+    }
+}
---
+++ Pilas/Pilas/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pilas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Definiendo Pila
+            Stack<string> nombres = new Stack<string>();
+
+            // Asignando valores a la Pila
+            Console.WriteLine("Ingresando datos en la Stack: ");
+            nombres.Push(Console.ReadLine());
+            nombres.Push(Console.ReadLine());
+            nombres.Push(Console.ReadLine());
+            nombres.Push(Console.ReadLine());
+            nombres.Push(Console.ReadLine());
+
+            // Muestra los datos ingresados en la Pila
+            Console.WriteLine("\nDatos que contiene la Stack: ");
+            foreach (string nombre in nombres)
+            {
+                Console.WriteLine(nombre);
+            }
+
+            // Procesando datos 
+            Console.WriteLine("\nElemento a eliminar: {0} ", nombres.Peek());
+            Console.WriteLine("Eliminando el elemento: {0}", nombres.Pop());
+            Console.WriteLine("Siguiente elemento a eliminar: {0} ", nombres.Peek()); 
+            Console.WriteLine("Eliminando el elemento: {0}", nombres.Pop());
+            Console.WriteLine("Ultimo elemento a eliminar: {0} ", nombres.Peek());
+            Console.WriteLine("Eliminando el elemento: {0}", nombres.Pop());
+
+            // Definiendo segunda Pila a partir de la primera
+            Stack<string> nombres2 = new Stack<string>(nombres.ToArray());
+
+            // Muestra los datos de la segunda Pila
+            Console.WriteLine("\nContenido de la Stack2 (primera copia): ");
+            foreach (string nombre in nombres2)
+            {
+                Console.WriteLine(nombre);
+            }
+
+            // Definiendo array, contiene una copia de la segunda Pila
+            string[] array = new string[nombres.Count * 2];
+            nombres.CopyTo(array, nombres.Count);
+
+            // Definiendo Pila tres
+            Stack<string> nombres3 = new Stack<string>(array);
+
+            Console.WriteLine("\nContenido de la Stack3 (segunda copia), con duplicados y nulos:");
+            foreach (string numero in nombres3)
+            {
+                Console.WriteLine(numero);
+            }
+
+            Console.WriteLine("Ingresa el elemento a verificar en la Stack2: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("\nVerificando si la Stack2 contiende el elemento: " + name);
+            Console.WriteLine("nombres2.Contains/La Stack2 contiene (" + (name + ") = {0}"), nombres2.Contains(name));
+            Console.WriteLine("nombres2.Clear()/Limpiando la Stack2"); nombres2.Clear();
+            Console.WriteLine("nombres2.Count/Valores de la Stack2: {0}", nombres2.Count);  
+        }
+    }
+}
